bc8cdf25a0a5772184e5dd3c2b433ad5f907bbb7
aruie <amazinghell@naver.com>
2019-11-12 23:19:51 +0900

    first succeed

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,3 +1,20 @@
 #include <jni.h>
 #include <string>
+#include <opencv2/opencv.hpp>
 
+
+
+using namespace cv;
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_cpptest_MainActivity_ConvertRGBtoGray(JNIEnv *env, jobject thiz,
+                                                       jlong mat_addr_input,
+                                                       jlong mat_addr_result) {
+
+    Mat matInput = *(Mat *) mat_addr_input;
+    Mat matOutput = *(Mat *) mat_addr_result;
+
+    matOutput = matInput + 250;
+    //cvtColor(matInput, matOutput, COLOR_RGBA2GRAY);
+}
